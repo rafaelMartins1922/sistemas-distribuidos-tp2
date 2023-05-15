11159d0b31054f86bc52d29a8062a7d08cee4ba3
BarbaraCerqueira <97547878+BarbaraCerqueira@users.noreply.github.com>
2023-05-15 19:08:12 -0300

    Feitas correções

--- Parte 1 - Somador/somador.c
+++ Parte 1 - Somador/somador.c
@@ -6,8 +6,6 @@
 #include <stdatomic.h>
 #include <time.h>
 
-#define NUMBER_VECTOR_LENGTH 10000000 // tamanho do vetor de números
-
 // Struct para organizar os argumentos usados pelas threads
 typedef struct {
     pthread_t thread_id;
@@ -56,103 +54,99 @@ void* thread_sum(void* arg) {
 
 int main(int argc, char** argv) {
     int status, n_threads, n_overloaded_threads;
-    long int chunk_size; 
+    long int chunk_size, vector_length; 
     long long int sum, correct_sum;
     int8_t* numbers;
     atomic_flag lock = ATOMIC_FLAG_INIT;
     ThreadArgs* thread_args;
-    double elapsed_time = 0;
+    double elapsed_time;
     clock_t start_time, end_time;
 
-    if (argc != 2) {
-        printf("Uso: %s <n_threads>\n", argv[0]);
+    if (argc != 3) {
+        printf("Uso: %s <vector_length> <n_threads>\n", argv[0]);
         exit(EXIT_FAILURE);
     }
 
-    n_threads = atoi(argv[1]);
-    chunk_size = floor(NUMBER_VECTOR_LENGTH / n_threads);
-    n_overloaded_threads = NUMBER_VECTOR_LENGTH % n_threads;
+    vector_length = atoi(argv[1]);
+    n_threads = atoi(argv[2]);
+    chunk_size = floor(vector_length / n_threads);
+    n_overloaded_threads = vector_length % n_threads;
 
-    // Repete a execução por 10 vezes para obter um tempo mais preciso
-    for (int exec_iter = 0; exec_iter < 10; exec_iter++) {
 
-        // Aloca o vetor de números aleatórios
-        numbers = (int8_t*) malloc(NUMBER_VECTOR_LENGTH * sizeof(int8_t));
-        if (numbers == NULL) {
-            printf("Erro: Não foi possível alocar memória para os números.\n");
-            exit(EXIT_FAILURE);
-        }
-
-        // Preenche o vetor com números aleatórios entre -100 e 100
-        srand(clock()); // Usado clock como seed devido ao tempo curto das iterações em alguns casos
-        for (long int i = 0; i < NUMBER_VECTOR_LENGTH; i++) {
-            numbers[i] = rand() % 201 - 100;
-        }
-
-        // Aloca o array dos argumentos das threads
-        thread_args = (ThreadArgs*) malloc(n_threads * sizeof(ThreadArgs));
-        if (thread_args == NULL) {
-            printf("Erro: Não foi possível alocar memória para argumentos das threads.\n");
-            exit(EXIT_FAILURE);
-        }
-
-        // Inicializa a estrutura de argumentos das threads sem o ID
-        for (int i = 0; i < n_threads; i++) {
-            thread_args[i].thread_index = i;
-            thread_args[i].num_threads = n_threads;
-            thread_args[i].n_overloaded_threads = n_overloaded_threads;
-            thread_args[i].numbers = numbers;
-            thread_args[i].lock = &lock;
-            thread_args[i].sum = &sum;
-
-            if (i < n_overloaded_threads) {
-                thread_args[i].chunk_size = chunk_size + 1;
-            }
-            else {
-                thread_args[i].chunk_size = chunk_size;
-            }
-        }
-
-        // Cria as threads e calcula a soma utilizando elas
-        start_time = clock();
-        sum = 0;
-        for (int i = 0; i < n_threads; i++) {
-            status = pthread_create(&thread_args[i].thread_id, NULL, &thread_sum, &thread_args[i]);
-            if (status != 0) {
-                printf("Erro: Não foi possível criar threads.");
-                exit(EXIT_FAILURE);
-            }
-        }
-        for (int i = 0; i < n_threads; i++) {
-            status = pthread_join(thread_args[i].thread_id, NULL);
-            if (status != 0) {
-                printf("Erro: Não foi possível aguardar a finalização das threads.");
-                exit(EXIT_FAILURE);
-            }
-        }
-        end_time = clock();
-
-        // Calcula a soma correta e verifica o resultado
-        correct_sum = 0;
-        printf("Soma %d: \n", exec_iter+1);
-        for (long int i = 0; i < NUMBER_VECTOR_LENGTH; i++) {
-            correct_sum += numbers[i];
-        }
-        if (sum != correct_sum) {
-            printf("Erro: a soma está incorreta (esperado %lld, obtido %lld).\n",
-                    correct_sum, sum);
-        } else {
-            printf("Sucesso.\nResultado: %lld\n\n", sum);
-        }
-
-        elapsed_time += (double)(end_time - start_time) / CLOCKS_PER_SEC;
-
-        free(numbers);
-        free(thread_args);
+    // Aloca o vetor de números aleatórios
+    numbers = (int8_t*) malloc(vector_length * sizeof(int8_t));
+    if (numbers == NULL) {
+        printf("Erro: Não foi possível alocar memória para os números.\n");
+        exit(EXIT_FAILURE);
     }
 
-    elapsed_time = (elapsed_time / 10) * 1e3;
-    printf("Tempo médio de execução: %.5f ms\n", elapsed_time);
+    // Preenche o vetor com números aleatórios entre -100 e 100
+    srand(clock()); // Usado clock como seed devido ao tempo curto das iterações em alguns casos
+    for (long int i = 0; i < vector_length; i++) {
+        numbers[i] = rand() % 201 - 100;
+    }
+
+    // Aloca o array dos argumentos das threads
+    thread_args = (ThreadArgs*) malloc(n_threads * sizeof(ThreadArgs));
+    if (thread_args == NULL) {
+        printf("Erro: Não foi possível alocar memória para argumentos das threads.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    // Inicializa a estrutura de argumentos das threads sem o ID
+    for (int i = 0; i < n_threads; i++) {
+        thread_args[i].thread_index = i;
+        thread_args[i].num_threads = n_threads;
+        thread_args[i].n_overloaded_threads = n_overloaded_threads;
+        thread_args[i].numbers = numbers;
+        thread_args[i].lock = &lock;
+        thread_args[i].sum = &sum;
+
+        if (i < n_overloaded_threads) {
+            thread_args[i].chunk_size = chunk_size + 1;
+        }
+        else {
+            thread_args[i].chunk_size = chunk_size;
+        }
+    }
+
+    // Cria as threads e calcula a soma utilizando elas
+    start_time = clock();
+    sum = 0;
+    for (int i = 0; i < n_threads; i++) {
+        status = pthread_create(&thread_args[i].thread_id, NULL, &thread_sum, &thread_args[i]);
+        if (status != 0) {
+            printf("Erro: Não foi possível criar threads.");
+            exit(EXIT_FAILURE);
+        }
+    }
+    for (int i = 0; i < n_threads; i++) {
+        status = pthread_join(thread_args[i].thread_id, NULL);
+        if (status != 0) {
+            printf("Erro: Não foi possível aguardar a finalização das threads.");
+            exit(EXIT_FAILURE);
+        }
+    }
+    end_time = clock();
+
+    // Calcula a soma correta e verifica o resultado
+    correct_sum = 0;
+    for (long int i = 0; i < vector_length; i++) {
+        correct_sum += numbers[i];
+    }
+    if (sum != correct_sum) {
+        printf("Erro: a soma está incorreta (esperado %lld, obtido %lld).\n",
+                correct_sum, sum);
+    } else {
+        printf("Sucesso.\nResultado: %lld\n\n", sum);
+    }
+
+    elapsed_time = ((double)(end_time - start_time) / CLOCKS_PER_SEC) * 1e3;
+
+    free(numbers);
+    free(thread_args);
+
+    printf("Tempo de execução: %.5f ms\n", elapsed_time);
 
     exit(EXIT_SUCCESS);
 }
